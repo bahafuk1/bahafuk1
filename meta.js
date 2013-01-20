a9404225adebf93b08d2ed9408f9f8316f9eb3bc
bahafuk1 <bahafuk1@yahoo.com.tw>
2013-01-19 21:54:13 -0800

    Create meta.js

---
+++ meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name       Korean ShXt Blocker
+// @namespace  http://home.gamer.com.tw/document
+// @version    1.2.1
+// @description  Auto block korean XX in Bahamut
+// @match      http://forum.gamer.com.tw/B.php*
+// @copyright  2013+, http://home.gamer.com.tw/document
+// @updateURL  https://raw.github.com/bahafuk1/bahafuk1/gh-pages/meta.js
+// @downloadURL https://raw.github.com/bahafuk1/bahafuk1/gh-pages/ksb.js
+// ==/UserScript==
