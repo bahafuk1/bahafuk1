495c81317a42034d3da81be75e8fef261afa06ad
bahafuk1 <bahafuk1@yahoo.com.tw>
2013-01-20 13:15:26 +0800

    Update ksb.js

--- ksb.js
+++ ksb.js
@@ -3,7 +3,7 @@
 // @namespace  http://home.gamer.com.tw/document
 // @version    1.0
 // @description  Auto block korean XX in Bahamut
-// @match      http://forum.gamer.com.tw/B.php
+// @match      http://forum.gamer.com.tw/B.php*
 // @copyright  2013+, http://home.gamer.com.tw/document
 // @updateURL  https://raw.github.com/bahafuk1/bahafuk1/gh-pages/ksb.js
 // ==/UserScript==
